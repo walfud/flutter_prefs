e56943aadfed5f64c25f5c72a84c8df934e9bc39
walfud <hi@walfud.com>
2019-02-27 15:31:38 +0800

    memory cache

--- lib/prefs.dart
+++ lib/prefs.dart
@@ -1,9 +1,6 @@
 import 'dart:async';
 
-import 'package:flutter/services.dart';
-
 class Prefs {
-  static const MethodChannel _channel = const MethodChannel('prefs');
   static const String _spliter = '.';
 
   static Prefs _sInstance;
@@ -16,35 +13,57 @@ class Prefs {
   }
 
   String name;
-  Prefs(this.name);
+  Prefs(this.name) {
+    // TODO: sqlite read
+  }
 
   Map<String, Object> cache = new Map<String, Object>();
-  void setValue(String key, Object value) {
+  Future<Object> setValue(String key, Object value) {
+    var path = _parsePath(key);
+
+    // Construct path table
+    Map<String, Object> currTable = cache;
+    for (var i in path.sublist(0, path.length - 1)) {
+      if (currTable[i] is! Map) {
+        // New path or Overwrite original leaf
+        currTable[i] = new Map<String, Object>();
+      }
+
+      currTable = currTable[i];
+    }
+
+    // Mount value on leaf
+    String leaf = path.last;
+    currTable[leaf] = value;
+
+    // TODO: sqlite write
+
+    return new Future.value(null);
+  }
+
+  Object getValue(String key) {
+    var path = _parsePath(key);
+
+    //
+    Map<String, Object> currTable = cache;
+    for (var i in path.sublist(0, path.length - 1)) {
+      if (currTable[i] is! Map) {
+        // New path or Overwrite original leaf
+        return null;
+      }
+
+      currTable = currTable[i];
+    } 
+
+    String leaf = path.last;
+    return currTable[leaf];
+  }
+
+  List<String> _parsePath(String key) {
     if (key == null || key.isEmpty) {
       throw ArgumentError('`key` MUST NOT empty');
     }
 
-    final keys = key.split(_spliter);
-
-    // Construct path
-    Map<String, Object> currTable = cache;
-    for (var path in keys.sublist(0, keys.length - 1)) {
-      if (currTable[path]?.runtimeType != Map) {
-        // New path or Overwrite original leaf
-        currTable[path] = new Map<String, Object>();
-      }
-
-      currTable = currTable[path];
-    }
-
-    // Mount value on leaf
-    String leaf = keys.last;
-    currTable[leaf] = value;
-
-    return;
-  }
-
-  Object getValue(String key) {
-    return null;
+    return key.split(_spliter);
   }
 }
---
+++ test/prefs_test.dart
@@ -0,0 +1,40 @@
+import 'package:test/test.dart';
+import 'package:prefs/prefs.dart';
+
+void main() {
+  group('Prefs', () {
+    Prefs prefs;
+
+    setUp(() async {
+      prefs = Prefs.defaultInstance();
+    });
+
+    tearDown(() {});
+
+    test('leaf', () async {
+      prefs.setValue('foo', 123);
+      expect(prefs.getValue('foo'), 123);
+
+      prefs.setValue('bar', 'song');
+      prefs.setValue('bar', 'walfud'); // over write
+      expect(prefs.getValue('bar'), 'walfud');
+    });
+
+    test('path', () async {
+      prefs.setValue('a', 1);
+      expect(prefs.getValue('a'), 1);
+
+      // path over write
+      prefs.setValue('a.b', 12);
+      expect(prefs.getValue('a.b'), 12);
+
+      prefs.setValue('a.c', 13);
+      expect(prefs.getValue('a.c'), 13);
+
+      // path value
+      Map<String, Object> pathValue = prefs.getValue('a');
+      expect(pathValue['b'], 12);
+      expect(pathValue['c'], 13);
+    });
+  });
+}
