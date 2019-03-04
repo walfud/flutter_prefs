a32286e573de67d1dffdda33a6303f27bfbae3be
walfud <hi@walfud.com>
2019-03-04 19:54:04 +0800

    compare to shared_preferences.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'package:prefs/prefs.dart';
 
@@ -15,25 +16,102 @@ class _MyAppState extends State<MyApp> {
   String _output = '';
   List<String> _tips = [];
   Prefs _prefs;
+  SharedPreferences _sharedPreferences;
+  LibMethodFactory _methodFactory;
+  LibMethodFactory _prefsMethodFactory, _sharedpreferencesMethodFactory;
 
   @override
   void initState() {
     super.initState();
+    _prefsMethodFactory = _methodFactory = new LibMethodFactory(
+      onSet: () async {
+        int debugId = nextDebugId();
+        final start = DateTime.now();
+        Future<void> res = _prefs
+            .setValue(_inputKey, _inputValue, debugId: debugId);
+        final end = DateTime.now();
+        await res;
+        final awaitEnd = DateTime.now();
+        setState(() {
+          final cost = end.millisecondsSinceEpoch - start.millisecondsSinceEpoch;
+          final awaitCost = awaitEnd.millisecondsSinceEpoch - start.millisecondsSinceEpoch;
+          _tips.add('$debugId'.padLeft(5) + ' -> ' + 'prefs set: cache cost $cost ms, persist cost $awaitCost ms');
+        });
+      },
+      onGet: () {
+        final start = DateTime.now();
+        final res = _prefs.getValue(_inputKey);
+        final end = DateTime.now();
+        setState(() {
+          _output = res.toString();
+
+          int debugId = nextDebugId();
+          final cost = end.millisecondsSinceEpoch - start.millisecondsSinceEpoch;
+          _tips.add('$debugId'.padLeft(5) + ' -> ' + 'prefs get: cost: $cost ms');
+        });
+      },
+    );
+    _sharedpreferencesMethodFactory = new LibMethodFactory(
+      onSet: () async {
+        int debugId = nextDebugId();
+        final start = DateTime.now();
+        Future<void> res = _sharedPreferences.setString(
+            _inputKey, _inputValue);
+        final end = DateTime.now();
+        await res;
+        final awaitEnd = DateTime.now();
+        setState(() {
+          final cost = end.millisecondsSinceEpoch - start.millisecondsSinceEpoch;
+          final awaitCost = awaitEnd.millisecondsSinceEpoch - start.millisecondsSinceEpoch;
+          _tips.add('$debugId'.padLeft(5) + ' -> ' + 'shared_preferences set: cache cost $cost ms, persist cost $awaitCost ms');
+        });
+      },
+      onGet: () {
+        final start = DateTime.now();
+        final res = _sharedPreferences.getString(_inputKey);
+        final end = DateTime.now();
+        setState(() {
+          _output = res.toString();
+
+          int debugId = nextDebugId();
+          final cost = end.millisecondsSinceEpoch - start.millisecondsSinceEpoch;
+          _tips.add('$debugId'.padLeft(5) + ' -> ' + 'shared_preferences get: cost: $cost ms');
+        });
+      },
+    );
 
     Prefs.setDebugListener((debugId, desc, [obj]) {
       setState(() {
-        _tips.add('$debugId'.padLeft(5) + ':' + desc);
+        _tips.add('$debugId'.padLeft(5) + ' -> ' + desc);
       });
     });
-    Prefs.defaultInstance(debugId: nextDebugId())
-        .then((prefs) {
-            _prefs = prefs;
 
-            setState(() {
-              _inputKey = 'foo';
-              _inputValue = '1234';
-            });
-        });
+    final initializeStartTime = DateTime.now();
+    Prefs.defaultInstance(debugId: nextDebugId()).then((prefs) {
+      final initializeEndTime = DateTime.now();
+      setState(() {
+        int debugId = nextDebugId();
+        final cost = initializeEndTime.millisecondsSinceEpoch -
+            initializeStartTime.millisecondsSinceEpoch;
+        _tips.add(
+            '$debugId'.padLeft(5) + ' -> ' + 'prefs initialize: cost "$cost" ms');
+      });
+
+      _prefs = prefs;
+    });
+
+    final sharedPreferenceInitializeStartTime = DateTime.now();
+    SharedPreferences.getInstance().then((sharedPreferences) {
+      final sharedPreferenceInitializeEndTime = DateTime.now();
+      setState(() {
+        int debugId = nextDebugId();
+        final cost = sharedPreferenceInitializeEndTime.millisecondsSinceEpoch - sharedPreferenceInitializeStartTime.millisecondsSinceEpoch;
+        _tips.add(
+            '$debugId'.padLeft(5) + ' -> ' + 'shared_preferences initialize: cost "$cost" ms');
+      });
+
+      _sharedPreferences = sharedPreferences;
+    });
   }
 
   @override
@@ -45,6 +123,29 @@ class _MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: <Widget>[
+            // Compare
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
+                Expanded(
+                  child: RadioListTile(
+                    title: Text('prefs'),
+                    value: _prefsMethodFactory,
+                    groupValue: _methodFactory,
+                    onChanged: _onLibChange,
+                  ),
+                ),
+                Expanded(
+                  child: RadioListTile(
+                    title: Text('shared_preferences'),
+                    value: _sharedpreferencesMethodFactory,
+                    groupValue: _methodFactory,
+                    onChanged: _onLibChange,
+                  ),
+                ),
+              ],
+            ),
+
             // Input
             Row(
               children: <Widget>[
@@ -75,33 +176,11 @@ class _MyAppState extends State<MyApp> {
                   children: <Widget>[
                     RaisedButton(
                       child: Text('Set'),
-                      onPressed: () async {
-                        int debugId = nextDebugId();
-                        final start = DateTime.now();
-                        Future<void> res = _prefs.setValue(_inputKey, _inputValue, debugId: debugId);
-                        final end = DateTime.now();
-                        await res;
-                        final awaitEnd = DateTime.now();
-                        final cost = end.millisecondsSinceEpoch - start.millisecondsSinceEpoch;
-                        final awaitCost = awaitEnd.millisecondsSinceEpoch - start.millisecondsSinceEpoch;
-                        setState(() {
-                          _tips.add('$debugId'.padLeft(5) + ':' + 'set: cost $cost ms, persist cost $awaitCost ms');
-                        });
-                      },
+                      onPressed: _methodFactory.onSet,
                     ),
                     RaisedButton(
                       child: Text('Get'),
-                      onPressed: () {
-                        int debugId = nextDebugId();
-                        final start = DateTime.now();
-                        final res = _prefs.getValue(_inputKey);
-                        final end = DateTime.now();
-                        final cost = end.millisecondsSinceEpoch - start.millisecondsSinceEpoch;
-                        setState(() {
-                          _output = res.toString();
-                          _tips.add('$debugId'.padLeft(5) + ':' + 'get: cost: $cost ms');
-                        });
-                      },
+                      onPressed: _methodFactory.onGet,
                     ),
                   ],
                 ),
@@ -132,7 +211,22 @@ class _MyAppState extends State<MyApp> {
     );
   }
 
+  void _onLibChange(LibMethodFactory factory) => setState(() {
+        _methodFactory = factory;
+      });
+
   // Debug
   static int _debugId = 0;
+
   int nextDebugId() => ++_debugId;
+}
+
+class LibMethodFactory {
+  Function onSet;
+  Function onGet;
+
+  LibMethodFactory({
+    this.onSet,
+    this.onGet,
+  });
 }
